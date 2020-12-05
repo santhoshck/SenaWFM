f6cfef49e3fef61b2c1ebafac4c00882d6ad3f4a
CK <santhosh.ck@gmail.com>
2020-12-05 06:48:04 +0530

    Set LOGIN_REDIRECT_URL

--- vsena/settings.py
+++ vsena/settings.py
@@ -120,6 +120,7 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
