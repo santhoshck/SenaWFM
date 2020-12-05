26a4fafab93f54e663af81b11dbddba1b3c2a773
CK <santhosh.ck@gmail.com>
2020-12-05 21:13:30 +0530

    Added Login Required

--- organization/views.py
+++ organization/views.py
@@ -3,27 +3,31 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Person, OrgUnit
 from .forms import OUForm
 
 #TODO
 #PermissionDeniedView - Check the views documentation
 
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = 'organization/home.html'
     model = OrgUnit
     context_object_name ='ou_list' #This is the object_list seen by template
 
-class PersonDetailView(generic.DetailView):
+class PersonDetailView(LoginRequiredMixin, generic.DetailView):
     model = Person
     template_name = 'organization/person_detail.html'
     context_object_name = 'person'
 
-class OUDetailView(generic.DetailView):
+class OUDetailView(LoginRequiredMixin, generic.DetailView):
     model = OrgUnit
     template_name = 'organization/ou_detail.html' 
     context_object_name ='ou'
 
+@login_required
 def ou_edit (request, ou_id):
     ou = get_object_or_404(OrgUnit, pk=ou_id)
     if request.method == 'POST':
@@ -35,6 +39,7 @@ def ou_edit (request, ou_id):
         form = OUForm(instance=ou)
     return render (request, 'organization/ou_edit.html', {'form':form, 'ou':ou}) 
 
+@login_required
 def ou_create (request):
     if request.method == 'POST':
         form = OUForm (request.POST)    
