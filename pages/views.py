be2eaa74f67845ce34b37cad88742e698af5c6f4
CK <santhosh.ck@gmail.com>
2020-12-24 19:01:30 +0530

    Home page update

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 
-from organization.models import OrgUnit
+from organization.models import OrgUnit,Employee
 
 @login_required
 def home_view (request):
-    change_history= OrgUnit.history.all()
-    context = {'change_history': change_history}
+    ou_changes= OrgUnit.history.all()
+    emp_changes= Employee.history.all()
+    context = {
+        'ou_changes': ou_changes,
+        'emp_changes': emp_changes
+        }
     return render(request, 'pages/home.html', context)
